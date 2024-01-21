96da582503cb250bede9c2819e25df0e104f1974
Brandon Hogan <bhogandev@outlook.com>
2024-01-21 12:07:31 -0500

    Initial Commit

---
+++ Zapped Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Zapped Watch App
+//
+//  Created by Brandon Hogan on 1/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Zapped Watch App/ZappedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZappedApp.swift
+//  Zapped Watch App
+//
+//  Created by Brandon Hogan on 1/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Zapped_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
